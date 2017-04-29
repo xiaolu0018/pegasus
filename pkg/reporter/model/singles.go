5e2d4631085d7270153b112ce652acdfce2f2100
panxy <1851616111@qq.com>
2017-04-29 13:48:53 +0800

    reporter 单页报告图片地址去重

--- pkg/reporter/model/singles.go
+++ pkg/reporter/model/singles.go
@@ -7,12 +7,18 @@ import (
 
 func parseSingles(s *string) []types.Single {
 	l := []types.Single{}
+	reduceM := map[string]struct{}{}
 	sl := strutil.ClipDBArray(s)
 	for _, str := range sl {
 		i := types.Single{}
 		items := strutil.ClipDBObject(&str)
+		if _, exist := reduceM[items[0]]; exist {
+			continue
+		}
+
 		i.Image = items[0]
 		l = append(l, i)
+		reduceM[items[0]] = struct{}{}
 	}
 
 	return l
