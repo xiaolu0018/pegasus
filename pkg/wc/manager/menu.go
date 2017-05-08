c6a278d32f73edbf7e6348115bdef92008466509
kangxd <77kxd2008@163.com>
2017-05-08 17:12:09 +0800

    fix wc 改为微信预约体检

--- pkg/wc/manager/menu.go
+++ pkg/wc/manager/menu.go
@@ -7,7 +7,7 @@ import (
 	wcpitoken "github.com/1851616111/util/weichat/util/api-token"
 )
 
-func CreateMenu(appid, schema, domain string) error {
+func CreateMenuForActivity(appid, schema, domain string) error {
 	appoint2 := fmt.Sprintf("https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s&redirect_uri=%s%%3a%%2f%%2f%s%%2fapi%%2factivity%%2findex&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect",
 		appid, schema, domain)
 
@@ -19,5 +19,14 @@ func CreateMenu(appid, schema, domain string) error {
 	//button := menu.NewTopButton("迪安").AddSub(bt1).AddSub(bt2).AddSub(bt3)
 
 	button := menu.NewTopButton("活动").AddSub(bt1)
+	return menu.CreateMenu(button, wcpitoken.TokenCtrl.GetToken())
+}
+
+func CreateMenu() error {
+	appoint := `https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxd09c7682905819e6&redirect_uri=http%3a%2f%2fwww.elepick.com%2fapi%2fappoint&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect`
+	appoint3 := `https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxd09c7682905819e6&redirect_uri=http%3a%2f%2fwww.elepick.com%2fapi%2fbranch&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect`
+	appoint2 := `https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxd09c7682905819e6&redirect_uri=http%3a%2f%2fwww.elepick.com%2fapi%2freportmenu&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect`
+	bt1, bt2, bt3 := menu.NewViewButton("预约体检", appoint), menu.NewViewButton("我的报告", appoint2), menu.NewViewButton("分院", appoint3)
+	button := menu.NewTopButton("迪安").AddSub(bt1).AddSub(bt2).AddSub(bt3)
 	return menu.CreateMenu(button, wcpitoken.TokenCtrl.GetToken())
 }
