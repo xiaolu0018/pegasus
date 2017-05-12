0a2c6296bd49ca93579008441f6e83f6ce71d7cf
panxy <1851616111@qq.com>
2017-05-12 15:42:29 +0800

    更换微信事件推送功能

--- pkg/wc/manager/activity.go
+++ pkg/wc/manager/activity.go
@@ -10,7 +10,7 @@ import (
 func WatchEvent(appid, schema, domain string) error {
 	url := "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxadb8865cdb995ded&redirect_uri=http://hd1.dahe100.cn/api/activity/index&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect"
 
-	imgUrl := "http://hd1.dahe100.cn/dist/activity/img/head.png"
+	imgUrl := "http://hd1.dahe100.cn/dist/activity/img/pushmsg.png"
 	article := event.NewArticleAction("跟衰老 Say Bye Bye", "晒合影, 喊好友来助力！！！", imgUrl, url)
 	handler.EventManager.RegistAction(event.E_Subscribe, article)
 	handler.EventManager.RegistAction(event.E_UnSubscribe, article)
