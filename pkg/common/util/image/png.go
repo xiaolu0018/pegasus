780ddf3084a09f8c1b7bd74ca35b8b2054702107
panxy <1851616111@qq.com>
2017-05-13 12:13:25 +0800

    添加文件

--- pkg/common/util/image/png.go
+++ pkg/common/util/image/png.go
@@ -17,7 +17,8 @@ func GemPersonPngPic(path, name, company, pic_person, pic_comeon, pic_erweima, p
 		return "", err
 	}
 
-	target := fmt.Sprintf("%d_%s.png", time.Now().UnixNano(), rand.String(20))
+	targetName  := fmt.Sprintf("%d_%s.png", time.Now().UnixNano(), rand.String(20))
+	target := fmt.Sprintf("%s/%s", path, targetName)
 	file, err := os.Create(target) //需生成的文件名
 	if err != nil {
 		return "",fmt.Errorf(err.Error() + "create err")
@@ -109,5 +110,5 @@ func GemPersonPngPic(path, name, company, pic_person, pic_comeon, pic_erweima, p
 		return "", err
 	}
 
-	return target, nil
+	return targetName, nil
 }
