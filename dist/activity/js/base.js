7d950b8dd07e245af2ea2acde03acc7a590876a2
panxy <1851616111@qq.com>
2017-05-13 16:24:50 +0800

    增加微信活动图片生成

--- dist/activity/js/base.js
+++ dist/activity/js/base.js
@@ -1,5 +1,5 @@
 var baseUrl="http://hd1.dahe100.cn";
-var weLink="http://weixin.qq.com/r/VUiQiMrEpJfbrY7r9x1H";
+var weLink="http://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzAwNDE4OTgyNw==#wechat_webview_type=1&wechat_redirect";
 //提示框显示
 var $toast = $('#toast');
 function showToast(htdata,myclass){
